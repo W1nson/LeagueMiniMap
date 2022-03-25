b6b2d5cdffa9edb82dc0bb683802402c920ed2f1
jeffreyw9998 <78579425+jeffreyw9998@users.noreply.github.com>
2022-03-25 13:00:25 -0700

    function to get champ winrate and a list of counters

--- leagueAPI.py
+++ leagueAPI.py
@@ -1,14 +1,30 @@
 import json 
 import requests
+from bs4 import BeautifulSoup
+
+
+def getChampionWinRate(name):
+    result = requests.get(f'https://app.mobalytics.gg/lol/champions/{name}/counters')
+    soup = BeautifulSoup(result.content, 'html.parser')
+    divTag = soup.find_all("div", {"class":"m-89mhyo"})
+    tbodyTag = divTag[0].find_all("tbody", {"class":"m-wsbm9"})
+    trTag = tbodyTag[0].find_all("tr", {"class":"m-1pkzzoz"})
+    tdTag = trTag[1].find_all("td", {"class":"m-m7fsih"})
+    return tdTag[0].getText()
 
 
 
+def getChampionCounters(name):
+    counters = dict()
+    result = requests.get(f'https://app.mobalytics.gg/lol/champions/{name}/counters')
+    soup = BeautifulSoup(result.content, "html.parser")
+    divTag = soup.find_all("div", {"class":"m-1p8lnhy"})
+    for i in divTag:
+        counter_name = i.find_all("div",{"class":"m-1j4udfm"})[0].getText()
+        counter_wr = i.find_all("div", {"class":"m-oft0zz"})[0].getText()
+        counters[counter_name] = counter_wr
+    return counters
 
+print(getChampionCounters("zeri"))
+print(getChampionWinRate("zeri"))
 
-result = requests.get("https://app.mobalytics.gg/lol/champions/akshan/counters")
-
-# json_string = json.dumps(result.json)
-print(result.json)
-with open('normal.html', 'wb') as outfile:
-    outfile.write(result.content)
-# print(result.content) 
